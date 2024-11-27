d8f80b39e41be079e5cd94349ccfcaa223bd1529
spareXcode <ayush.s@sparecare.in>
2024-11-27 12:52:36 +0530

    dashboard api fectch working on it

--- src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -11,109 +11,131 @@ import { flatMap } from 'rxjs';
 @Component({
   selector: 'app-admin-dashboard',
   standalone: true,
-  imports: [TopNavBarComponent,OrderRequestStatusCardComponent,SideMenueComponent,ReactiveFormsModule,CommonModule],
+  imports: [
+    TopNavBarComponent,
+    OrderRequestStatusCardComponent,
+    SideMenueComponent,
+    ReactiveFormsModule,
+    CommonModule,
+  ],
   templateUrl: './admin-dashboard.component.html',
-  styleUrl: './admin-dashboard.component.css'
+  styleUrl: './admin-dashboard.component.css',
 })
 export class AdminDashboardComponent {
+  constructor(private adminDashBoardService: AdmindashboardService) {}
 
-  constructor(private adminDashBoardService: AdmindashboardService ){}
-  
-  AdminDashboardInputData : FormGroup = new FormGroup({
+  AdminDashboardInputData: FormGroup = new FormGroup({
     BrandID: new FormControl(''),
-    DealerID: new FormControl({value:'',disabled:true}),
-    LocationID: new FormControl({value:'',disabled:true}),
-    TypeOfOrder:new FormControl(''),
+    DealerID: new FormControl({ value: '', disabled: true }),
+    LocationID: new FormControl({ value: '', disabled: true }),
+    TypeOfOrder: new FormControl(''),
     ApproveBy: new FormControl(''),
-    Timeband:new FormControl('')
-  })
-  isbrandLoaded = false
-  isDealerloaded = false
-  isLocationLoaded = false
+    Timeband: new FormControl(''),
+  });
+  isbrandLoaded = false;
+  isDealerloaded = false;
+  isLocationLoaded = false;
 
-  AdminDashboardData: any = []
+  AdminDashboardData: any = [];
 
-  onSelctionChangeBrand(){
-    this.AdminDashboardData = this.AdminDashboardInputData.value
+  onSelctionChangeBrand() {
+    this.AdminDashboardData = this.AdminDashboardInputData.value;
     console.log(this.AdminDashboardData)
-    this.fetchDealerData(this.AdminDashboardInputData.value.BrandID)
-
+    this.fetchDealerData(this.AdminDashboardInputData.value.BrandID);
+    this.AdminDashboardInputData.get('DealerID')?.enable();
+    this.fetchDashBoardData(this.AdminDashboardData);
   }
-  onSelectionChangeDealer(){
-    this.AdminDashboardData = this.AdminDashboardInputData.value
-    this.fetchLocationData(this.AdminDashboardInputData.value.DealerID)
-    console.log("Dealer Data",this.AdminDashboardData)
-  }
-
-  onSelectionChangeApprovedBy(){
-    this.AdminDashboardData = this.AdminDashboardInputData.value
-    this.fetchApprovedByData(this.AdminDashboardInputData.value.LocationID)
-    console.log("Dealer Data",this.AdminDashboardData)
+  onSelectionChangeDealer() {
+    this.AdminDashboardData = this.AdminDashboardInputData.value;
+    this.fetchLocationData(this.AdminDashboardInputData.value.DealerID);
+    // console.log("Dealer Data",this.AdminDashboardData)
+    this.AdminDashboardInputData.get('LocationID')?.enable();
   }
 
-  ngOnInit(): void {
-    this.fetchBrandData()
-    
-    
-    
-  }
-  isLoading = false
+  onSelectionChangeApprovedBy() {
+    this.AdminDashboardData = this.AdminDashboardInputData.value;
+    this.fetchApprovedByData(this.AdminDashboardInputData.value.LocationID);
 
-  BrandData: any = []
-  DealerData: any = []
-  LocationData: any = []
-  TypeOfOrderData: any = []
-  ApproveByData: any = []
+    // console.log("Dealer Data",this.AdminDashboardData)
+  }
+
   
+  isLoading = false;
 
-  fetchBrandData(){
-    this.isLoading = true
-    this.adminDashBoardService.getAdminBrandData().subscribe((res:any)=>{
-      this.BrandData = res.data
-      this.AdminDashboardInputData.get('DealerID')?.enable();
-      this.isLoading = false
-      console.log("BrandData",this.BrandData);
-    })
-    this.isLoading = false
+  BrandData: any = [];
+  DealerData: any = [];
+  LocationData: any = [];
+  TypeOfOrderData: any = [];
+  ApproveByData: any = [];
+
+  AdminDashBoardNumbers: any = [];
+
+  fetchBrandData() {
+    this.isLoading = true;
+    this.adminDashBoardService.getAdminBrandData().subscribe((res: any) => {
+      this.BrandData = res.data;
+      this.isLoading = false;
+      console.log('BrandData', this.BrandData);
+      
+    });
+    
+    this.isLoading = false;
   }
 
-  fetchDealerData(brand_id:any) {
+  fetchDealerData(brand_id: any) {
     this.isLoading = true;
     this.adminDashBoardService
       .getAdminDealersData({ brand_id: brand_id })
       .subscribe((res: any) => {
         this.DealerData = res.data;
-        this.AdminDashboardInputData.get('LocationID')?.enable();
         console.log('Dealer Data', this.DealerData);
       });
-      this.isLoading = false
-  } 
-
-  fetchLocationData(dealer_id:any){
-    this.isLoading = true
-    this.adminDashBoardService.getAdminLocationData({dealer_id: dealer_id}).subscribe((res:any)=>{
-      this.LocationData = res.data
-      this.isLoading = false
-      console.log("location Data ",this.LocationData)
-    })
-    this.isLoading = false
+    this.isLoading = false;
   }
 
-  fetchApprovedByData(location_id:any){
-    this.isLoading = true
-    this.adminDashBoardService.getAdminApprovedByData({location_id: location_id}).subscribe((res:any)=>{
-      this.ApproveByData = res.data
-      this.isLoading = false
-      console.log("ApproveByData ",this.ApproveByData)
-    })
-    this.isLoading = false
+  fetchLocationData(dealer_id: any) {
+    this.isLoading = true;
+    this.adminDashBoardService
+      .getAdminLocationData({ dealer_id: dealer_id })
+      .subscribe((res: any) => {
+        this.LocationData = res.data;
+        this.isLoading = false;
+        console.log('location Data ', this.LocationData);
+      });
+    this.isLoading = false;
   }
 
-  fetchDashBoardData(data:any){
-    this.isLoading = true
-    this.adminDashBoardService.getAdminDashboardData({})
+  fetchApprovedByData(location_id: any) {
+    this.isLoading = true;
+    this.adminDashBoardService
+      .getAdminApprovedByData({ location_id: location_id })
+      .subscribe((res: any) => {
+        this.ApproveByData = res.data;
+        console.log('ApproveByData ', this.ApproveByData);
+      });
+    this.isLoading = false;
   }
 
-  
+  fetchDashBoardData(data: any) {
+    this.isLoading = true;
+    console.log("data ",data)
+    this.adminDashBoardService
+      .getAdminDashboardData({
+        location_id: data?.LocationID,
+        orderType: data?.TypeOfOrder,
+        approvedBy: data?.ApproveBy,
+        timeBand: data?.Timeband,
+      })
+      .subscribe((res: any) => {
+        this.AdminDashBoardNumbers = res.data
+      });
+      this.isLoading = false;
+      console.log("dashboard data ", this.AdminDashBoardNumbers)
+  }
 
+
+  ngOnInit(): void {
+    this.fetchBrandData();
+   
+  }
 }
